eef62f857ca46c94a87312c8912174e5bf15612c
allisnone <zgx20022002@163.com>
2018-09-25 14:22:32 +0800

    up

--- html.py
+++ html.py
@@ -2,26 +2,20 @@
 from config import URL_MAPPING,IMAGE_STATUS
 
 def get_summary_title_html(h3_content='综合评估',now_time='2018-09-24'):
-    
-    summary_title = '<div style="width:33%; text-align:left; font-size: 18px; \
-        color: #2A2C30; font-weight:500;">' + '<h3>%s</h3>' % h3_content + '</div> \
-        <div style="width:34%; text-align:center;font-size: 15px; color:#2A2C30; \
-        vertical-align: middle;">' + '<span> %s </span>' % now_time + '</div>'
+    summary_title = '''
+        <div style="width:100%;font-size: 18px; color: #2A2C30; font-weight:500;">
+            <h3 style="text-align:left;">{0}</h3>  
+            <h5 style="text-align:right;">{1}</h5>
+        </div>'''.format( h3_content,now_time)
     return summary_title
 
 def get_L1_summary_html(h4_content='安全评估',loader_img='../static/images/loader.png',fail_img=IMAGE_STATUS['fail'],fail_count=0,
                         pass_img=IMAGE_STATUS['pass'],pass_count=0):
-    h4_content_html = '<h4>%s</h4>' % h4_content
-    loader_img_html = '<img id="securityGradeLoader" src="%s" style="width:60px; height:60px; margin:12px 0;">' % loader_img
-    fail_img_html = '<img src="%s" style="height:16px; width:16px; margin: 26px 6px 8px 0px;">' % fail_img
-    fail_count_html = '<p id="FailCountSecurity" style="color:#D82D24; font-size:20px; display:inline; top:13px; position:relative;">%s</p>' % fail_count
-    pass_img_html = '<img src="%s" style="height:16px; width:16px; margin: 26px 6px 8px 0px;">' % pass_img
-    pass_count_html = '<p id="PassCountSecurity" style="color:#399C1D; font-size:20px; display:inline; top:13px; position:relative;">%s</p>' % pass_count
     pass_rate = 0
     result_class = 'text_primary'
     
     if (fail_count + pass_count)>0:
-        pass_rate = round(round(pass_count,2)/(fail_count + pass_count),3)*100
+        pass_rate = round(round(pass_count,2)/(fail_count + pass_count),2)*100
         if pass_rate<50:
             result_class = 'text-danger' #text-wairning
         elif pass_rate<80:
@@ -29,8 +23,9 @@ def get_L1_summary_html(h4_content='安全评估',loader_img='../static/images/l
         else:
             result_class = 'text-success' #text-wairning
     #<img src="{failimg}" style="height:16px; width:16px; margin: 26px 6px 8px 0px;">      
-    pass_result = "{0}%".format(str(pass_rate)[:4])
+    pass_result = "{0}%".format(str(pass_rate).split('.')[0])
     percentage_result = '<span class="{0}" style="display:block;font-size:20px">{1}</span>'.format(result_class,pass_result)
+    #col-xs-12 col-sm-12 col-md-4 col-lg-4
     l1_summary_html = ''' <div class="test-result-div col-xs-12 col-sm-12 col-md-4 col-lg-4" style="background-color:#ffffff;">
         <div class="horizontal-align" style="width:255px;">
             <div style="width:100%; text-align:center;">
@@ -39,25 +34,26 @@ def get_L1_summary_html(h4_content='安全评估',loader_img='../static/images/l
                 </span>
                 <div class="horizontal-align" style="margin: 10px 12px;">
                     <div style="display:flex;">
-                        {percentage_result}
+                        
                         <div style="margin: 0 0 0 8%; width: 35%;">
                             <img src="{failimg}" style="height:16px; width:16px; margin: 26px 6px 8px 0px;">
                             <p id="FailCountData" style="color:#D82D24; font-size:20px; display:inline; top:13px; position:relative;">{failcount}</p>
-                            <span style="display:block; color: #4C4F54; word-wrap: break-word;">Failed</span>
+                            <span style="display:block; color: #4C4F54; word-wrap: break-word;">失败</span>
                         </div>
-                        <div style="margin: 0 8% 0 0%; width:25%;">
+                        <div style="margin: 0 8% 0 0%; width:35%;">
                             <img src="{passimg}" style="height:16px; width:16px; margin: 26px 6px 8px 0px;">
                             <p id="PassCountData" style="color:#399C1D; font-size:20px; display:inline; top:13px; position:relative;">{passcount}</p>
-                            <span style="display:block; color: #4C4F54;">Passed</span>
+                            <span style="display:block; color: #4C4F54;">成功</span>
+                        </div>
+                        <div style="margin: 0 8% 0 0%; width:35%;">
+                            <p id="PassCountData" style="color:#399C1D; height:16px; width:16px; margin: 26px 6px 8px 0px; font-size:20px;text-align:left;">{percentage_result}</p>
+                            <span style="display:block; color: #4C4F54;">成功率</span>
                         </div>
                     </div>
                 </div>
-                <span style="display:None;">
-                    <p style="font-size: 11px; color:#77797C;" class="text-center">Apply to Data Protection test</p>
-                </span>
             </div>
         </div>
-        </div>\n'''.format(head=h4_content,percentage_result=percentage_result,failimg=fail_img,failcount=fail_count,passimg=pass_img,passcount=pass_count)
+        </div>\n'''.format(head=h4_content,percentage_result=percentage_result,ptgimg=pass_img,failimg=fail_img,failcount=fail_count,passimg=pass_img,passcount=pass_count)
         #%(h4_content,loader_img,fail_img,fail_count,pass_img,pass_count)
     
     return l1_summary_html
@@ -104,7 +100,7 @@ def get_item_html(request_result,url_mapping=URL_MAPPING,image_status=IMAGE_STAT
     #test_description = URL_MAPPING[config_id]['description']
     
     
-    row_html = '''<div class="row {text_class}" style="display:flex; background-color:#ffffff;"> 
+    row_html = '''<div class="row {text_class}" style="display:flex;width:100%;background-color:#ffffff;"> 
         <div class="col-sm-1"> 
         <div> <img src="{status_img} "style="width:15px; height:15px; margin:5px;"> </div>
         </div> 
@@ -121,11 +117,11 @@ def get_item_html(request_result,url_mapping=URL_MAPPING,image_status=IMAGE_STAT
     return row_html;
 
 def form_head_element():
-    head_html =  '<div class="row"> \
+    head_html =  '<div class="row" style="width:100%;"> \
         <div class="col-sm-1"> <p> 结果 </p></div>  \
         <div class="col-sm-3"> <p> 测试分类 </p></div> \
         <div class="col-sm-1"> <p> 阻断 </p></div> \
-        <div class="col-sm-6"> <p> 测试描述 </p></div> \
+        <div class="col-sm-6" style="text-align:center;"> <p> 测试描述 </p></div> \
         <div class="col-sm-1"> <p> </p></div> \
         </div>'
     return  head_html
